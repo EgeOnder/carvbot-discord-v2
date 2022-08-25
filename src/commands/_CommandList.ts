576843718c2e0b2cf5710c95c0c6b07a6a6fb426
Ege Önder <40398628+EgeOnder@users.noreply.github.com>
2022-08-25 18:56:12 +0300

    tsc fix

--- src/commands/_CommandList.ts
+++ src/commands/_CommandList.ts
@@ -1,6 +1,5 @@
 import { Command } from '../types/Command';
 import { Ping } from './ping';
 import { Play } from './Play';
-import { Stop } from './Stop';
 
-export const CommandList: Command[] = [Ping, Play, Stop];
+export const CommandList: Command[] = [Ping, Play];
